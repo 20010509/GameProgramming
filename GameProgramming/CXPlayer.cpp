c8d13337b78e6cc57ba8478995c23f39ddfb7c5c
takahashi2001 <20010509@anabuki-net.ne.jp>
2021-07-12 11:54:49 +0900

    課題23

--- GameProgramming/CXPlayer.cpp
+++ GameProgramming/CXPlayer.cpp
@@ -2,12 +2,34 @@
 #include "CKey.h"
 
 void CXPlayer::Update(){
-	if (CKey::Push('W')){
-		ChangeAnimation(1, true, 60);
-		mPosition.mZ += 0.1;
+
+	//UŒ‚
+	if (CKey::Once(VK_SPACE)){
+		ChangeAnimation(3, false, 30);
 	}
-	else{
+	if (mAnimationIndex == 3 && mAnimationFrame >= mAnimationFrameSize){
+		ChangeAnimation(4, false, 30);
+	}
+	if (mAnimationIndex == 4 && mAnimationFrame >= mAnimationFrameSize){
 		ChangeAnimation(0, true, 60);
 	}
+
+	//ˆÚ“®
+	if (mAnimationIndex != 3 && mAnimationIndex != 4){
+		if (CKey::Push('A')){
+			mRotation.mY += 2;
+		}
+		if (CKey::Push('D')){
+			mRotation.mY -= 2;
+		}
+		if (CKey::Push('W')){
+			ChangeAnimation(1, true, 60);
+			mPosition += CVector(0.0f, 0.0f, 0.1f)*mMatrixRotate;
+		}
+		else{
+			ChangeAnimation(0, true, 60);
+		}
+	}
+
 	CXCharacter::Update();
 }
